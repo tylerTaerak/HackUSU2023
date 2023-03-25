7b70d74f0c6b1c5222bc34472e5d248490eb1b16
Preston Bell <37946248+prestonbell@users.noreply.github.com>
2023-03-25 10:27:34 -0600

    Update analysisModel.py

--- analysisModel.py
+++ analysisModel.py
@@ -13,38 +13,50 @@ rand_state=1000
 # read in the data
 df = pd.read_csv("/content/full_data_flightdelay.csv")
 
-def predictWeatherDelay():
-    # view target variable proportions
-    pd.crosstab(df['weather_delay'], df['weather_delay'], normalize='all')*100
+# see target variable proportions: clearly imbalanced
+pd.crosstab(df['DEP_DEL15'], df['DEP_DEL15'], normalize='all')*100
 
-    # prep training
-    y = df['weather_delay']
-    X = df.drop('weather_delay', axis=1, inplace= False)
+def predictWeatherDelay():
+    # choose independant and dependant vars
+    y = df['DEP_DEL15']
+    X = df.drop('DEP_DEL15', axis=1, inplace= False)
+    #or
+    #x= data_set.iloc[:, [2,3]].values  
+    #y= data_set.iloc[:, 4].values  
+
+    # testing/training split
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=rand_state)
 
-    # select logistic regression method
-    from sklearn.linear_model import LogisticRegression
+    # feature Scaling  
+    from sklearn.preprocessing import StandardScaler    
+    st_sc = StandardScaler()    
+    X_train= st_sc.fit_transform(X_train)    
+    X_test= st_sc.transform(X_test)  
 
-    # Fit Logistic Regression to the Training set
+    # Fitting Log. Reg. to training set
+    from sklearn.linear_model import LogisticRegression
     logistic = LogisticRegression()
     logistic.fit(X_train, y_train)
 
-    # testing segment
+    # Predicting Test Result
     y_hat = logistic.predict(X_test)
     y_hat_probs = logistic.predict_proba(X_test)[:,1] 
+    df_predictions = pd.DataFrame({'y_test': y_test, 'y_hat_probs': y_hat_probs, 
+                                   'y_hat': y_hat, 'y_hat_30': y_hat_30, 
+                                   'y_hat_60': y_hat_60})
 
-
+    # Testing model accuracy
     from sklearn.metrics import confusion_matrix
     from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, matthews_corrcoef
     from sklearn.metrics import confusion_matrix, classification_report, roc_curve, roc_auc_score
 
-    def logistic_report(X_train, y_train, X_test,y_test, threshold=0.5, penalty='none', class_weight=None): 
+    def logistic_report(X_train, y_train, X_test,y_test, threshold=0.4, penalty='none', class_weight=None): 
         logistic= LogisticRegression(class_weight=class_weight, penalty=penalty)
         logistic.fit(X_train, y_train)
         probs = logistic.predict_proba(X_test)[:,1]
         y_hat = np.where(probs>=threshold,1,0)
-        
+
         cm = confusion_matrix(y_test, y_hat)
         accuracy = round(accuracy_score(y_test,y_hat) ,2)
         precision = round(precision_score(y_test,y_hat),2)
@@ -52,7 +64,7 @@ def predictWeatherDelay():
         f1score = round(f1_score(y_test,y_hat),2)
         MCC = round(matthews_corrcoef(y_test,y_hat),2)
         cm_labled = pd.DataFrame(cm, index=['Actual : negative ','Actual : positive'], columns=['Predict : negative','Predict :positive '])
-        
+
         print("-----------------------------------------")
         print('Accuracy  = {}'.format(accuracy))
         print('Precision = {}'.format(precision))
@@ -62,7 +74,7 @@ def predictWeatherDelay():
         print("-----------------------------------------")
         return cm_labled
 
-    # threshold can be adjusted to meet need of end user
-    logistic_report(X_train, y_train, X_test,y_test, threshold=0.4)
-    logistic = LogisticRegression(penalty='none')
-    accuracy_CV5 = cross_val_score(estimator = logistic, X = X_train, y = y_train, cv = 5, scoring="accuracy")
+        # threshold can be adjusted to meet need of end user
+        logistic_report(X_train, y_train, X_test,y_test, threshold=0.4)
+        logistic = LogisticRegression(penalty='none')
+        accuracy_CV5 = cross_val_score(estimator = logistic, X = X_train, y = y_train, cv = 5, scoring="accuracy")
